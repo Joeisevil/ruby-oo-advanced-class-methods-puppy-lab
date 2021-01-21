40f9ed17429c4756b652c72cbee65c756b06daa8
Joe Downes-Berry <j.d.berry26@gmail.com>
2021-01-21 16:10:18 -0500

    commit tree 2d07e4d1ca4d3f6a7f8e2558546a6326bee9bbfb

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Add your code here
+require 'pry'
+
+class Dog
+    @@all = []
+    
+    attr_accessor :name, :print_all
+    def initialize(name)
+        @name=name
+        save       
+    end
+
+    def self.all
+        @@all
+    end
+
+    def Dog.clear_all
+        @@all.clear
+    end
+
+    def Dog.print_all
+        self.all.map do |dog| 
+            puts dog.name
+        end
+    end
+    
+    def save
+        @@all << self
+    end
+        # binding.pry
+    
+end
+
